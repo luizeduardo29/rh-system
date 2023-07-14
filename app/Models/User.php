be8f7283bb845ff54b40d8e10bc3077e861cd6c0
luizeduardo29 <davila.dudu@hotmail.com>
2023-07-14 14:50:31 -0300

    enums incomplete

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'initials',
+        'photo',
+        'birth',
+        'marital_status'
     ];
 
     /**
